40ecf54cc424f199cb0b44f6b99bd3ae205e6553
Orestil, Dudley <da191212@ens.uqam.ca>
2025-06-30 21:39:33 -0400

    Changement stats hebdo pour quotidien

--- lxt_dash/db_script.py
+++ lxt_dash/db_script.py
@@ -26,15 +26,29 @@ table_resultat_hebdo = """ CREATE TABLE IF NOT EXISTS resultats(
                        FOREIGN KEY (transcripteur_id) REFERENCES transcripteurs(id)
                        );"""
 
+table_resultat_quotidien = """ CREATE TABLE IF NOT EXISTS resultats_journaliers(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+transcripteur_id INTEGER NOT NULL,
+jour DATE NOT NULL,
+vitesse_h REAL,
+qualite REAL,
+productivite INTEGER,
+FOREIGN KEY (transcripteur_id) REFERENCES transcripteurs(id),
+UNIQUE(transcripteur_id, jour)
+);"""
+
 # FOREIGN KEY (transcripteur_id) REFERENCES transcripteurs(id)
 #Cette ligne signifie qu'un transcripteur_id doit obligatoirement correspondre à un id de la table transcripteur pour qu'une insertion soit valide
 
 
 #cursor.execute(table_transcripteur)
 #cursor.execute(table_resultat_hebdo)
-
+#cursor.execute(table_resultat_quotidien)
 #df = pd.read_csv("transcripteur.csv")
-df_res = pd.read_sql_query("select *from resultats", conn)
+#cursor.execute("""CREATE INDEX IF NOT EXISTS idx_transcripteur_jour
+#ON resultats_journaliers (transcripteur_id, jour);""")
+
+df_res = pd.read_sql_query("select *from resultats_journaliers", conn)
 print(df_res.tail())
 #df.columns = df.columns.str.lower()
 #df_transcripteurs = df[['id','nom','page_id','langue','actif']]
